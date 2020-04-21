16c29f3733c2c14edfc9bbd9f8841fe989b9a28c
Bubb13 <36863623+Bubb13@users.noreply.github.com>
2020-04-21 00:20:32 -0700

    Remove temp code and add sanity checks

--- EEex/copy/B3_Time.lua
+++ EEex/copy/B3_Time.lua
@@ -7,12 +7,6 @@ B3Time_PauseTick = 0
 B3Time_Flowing = false
 B3Time_FlowTick = -1
 
-function B3Time_Temp()
-	local g_pBaldurChitin = EEex_ReadDword(EEex_Label("g_pBaldurChitin"))
-	local m_pObjectGame = EEex_ReadDword(g_pBaldurChitin + EEex_Label("CBaldurChitin::m_pObjectGame"))
-	EEex_MessageBox(EEex_ToHex(m_pObjectGame + 0x2500))
-end
-
 function B3Time_TogglePause()
 	local worldScreenAddress = EEex_ReadDword(EEex_ReadUserdata(worldScreen))
 	EEex_Call(EEex_Label("CScreenWorld::TogglePauseGame"), {0, 0, 0, 1, 1}, worldScreenAddress, 0x0)
@@ -42,7 +36,7 @@ function B3Time_Tick()
 end
 
 function B3Time_KeyPressedListener(key)
-	if key == B3Time_Key and worldScreen:CheckIfPaused() then
+	if key == B3Time_Key and worldScreen == e:GetActiveEngine() and Infinity_TextEditHasFocus() == 0 and worldScreen:CheckIfPaused() then
 		B3Time_FlowTick = Infinity_GetClockTicks() + 500
 		B3Time_TogglePause()
 		B3Time_PauseScheduled = true
