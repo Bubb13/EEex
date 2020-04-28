4139b9802f3e30216dac2d264ef00f6e562ef4a9
Bubb13 <36863623+Bubb13@users.noreply.github.com>
2020-04-27 17:04:49 -0700

    Add B3Scale module

--- EEex/copy/EEex_INI.lua
+++ EEex/copy/EEex_INI.lua
@@ -5,6 +5,7 @@ EEex_Modules = {
 	["B3_Hotke"] = false,
 	["B3_Invis"] = false,
 	["B3_Portr"] = false,
+	["B3_Scale"] = false,
 	["B3_Time"]  = false,
 	["B3_Timer"] = false,
 	["ME_DAMAB"] = false,
