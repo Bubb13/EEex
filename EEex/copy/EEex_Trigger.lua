9520356a1d7f14917a4bf900d123ed2f1c846235
Bubb13 <36863623+Bubb13@users.noreply.github.com>
2022-06-22 12:34:01 -0700

    Reimplement trigger parsing functions
    - EEex_Trigger_ParseConditionalString()
    - EEex_Trigger_EvalScriptFileConditionalAsAIBase()
    - EEex_Trigger_EvalConditionalStringAsAIBase()

--- EEex/copy/EEex_Trigger.lua
+++ EEex/copy/EEex_Trigger.lua
@@ -1,4 +1,44 @@
 
+-------------
+-- General --
+-------------
+
+-- Parses the given string as a .BS trigger block and returns the compiled script object, (only filled with triggers).
+-- Call :free() on the returned CAIScriptFile when it is no longer needed.
+function EEex_Trigger_ParseConditionalString(string)
+	local toReturn = EEex_NewUD("CAIScriptFile")
+	toReturn:Construct()
+	toReturn.free = function(scriptFile)
+		scriptFile:Destruct()
+		EEex_FreeUD(scriptFile)
+	end
+	EEex_RunWithStackManager({
+		{ ["name"] = "cstring", ["struct"] = "CString", ["constructor"] = { ["args"] = { string } }, ["noDestruct"] = true } },
+		function(manager)
+			toReturn:ParseConditionalString(manager:getUD("cstring"))
+		end)
+	return toReturn
+end
+
+-- Evaluates compiled triggers returned by EEex_Trigger_ParseConditionalString() in the context of aiBase.
+function EEex_Trigger_EvalScriptFileConditionalAsAIBase(scriptFile, aiBase)
+	return scriptFile.m_curCondition:Hold(aiBase.m_pendingTriggers, aiBase) ~= 0
+end
+CAIScriptFile.evalConditionalAsAIBase = EEex_Trigger_EvalScriptFileConditionalAsAIBase
+
+-- Same as EEex_Trigger_EvalScriptFileConditionalAsAIBase() but takes a string instead of a compiled script object.
+-- Prefer using compiled triggers when efficiency is required.
+function EEex_Trigger_EvalConditionalStringAsAIBase(string, aiBase)
+	return EEex_RunWithStackManager({
+		{ ["name"] = "scriptFile", ["struct"] = "CAIScriptFile" },
+		{ ["name"] = "cstring", ["struct"] = "CString", ["constructor"] = { ["args"] = { string } }, ["noDestruct"] = true } },
+		function(manager)
+			local scriptFile = manager:getUD("scriptFile")
+			scriptFile:ParseConditionalString(manager:getUD("cstring"))
+			return scriptFile.m_curCondition:Hold(aiBase.m_pendingTriggers, aiBase) ~= 0
+		end)
+end
+
 -----------
 -- Hooks --
 -----------
