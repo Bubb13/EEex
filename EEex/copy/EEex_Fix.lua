29659395c5da200ef1a6e443d14f6534929ae016
Bubb13 <36863623+Bubb13@users.noreply.github.com>
2022-11-05 16:25:33 -0700

    Fix Spell() and SpellPoint() sometimes not being disruptable
    https://www.gibberlings3.net/forums/topic/34892-code-code-fixes-aka-stuff-we-cant-fix/?do=findComment&comment=307418

--- EEex/copy/EEex_Fix.lua
+++ EEex/copy/EEex_Fix.lua
@@ -19,3 +19,32 @@ function EEex_Fix_Hook_OnAddSpecialAbility(sprite, spell)
 			sprite:CheckQuickLists(abilityId, 1, 0, 0);
 		end)
 end
+
+----------------------------------------------------------------------------------
+-- Fix Spell() and SpellPoint() not being disruptable if the creature is facing --
+-- SSW(1), SWW(3), NWW(5), NNW(7), NNE(9), NEE(11), SEE(13), or SSE(15)         --
+----------------------------------------------------------------------------------
+
+function EEex_Fix_Hook_ShouldForceMainSpellActionCode(sprite, point)
+
+	local forcing = EEex_GetUDAux(sprite)["EEex_Fix_HasSpellOrSpellPointStartedCasting"] == 1
+
+	-- If I force the main spell action code, the direction-setting code
+	-- isn't run. Manually do that here so sprites still turn to face
+	-- their target after they have started the casting glow.
+	if forcing then
+		local message = EEex_NewUD("CMessageSetDirection")
+		message:Construct(point, sprite.m_id, sprite.m_id)
+		EngineGlobals.g_pBaldurChitin.m_cMessageHandler:AddMessage(message, 0)
+	end
+
+	return forcing
+end
+
+function EEex_Fix_Hook_OnSpellOrSpellPointStartedCastingGlow(sprite)
+	EEex_GetUDAux(sprite)["EEex_Fix_HasSpellOrSpellPointStartedCasting"] = 1
+end
+
+function EEex_Fix_Hook_OnSetCurrAction(sprite)
+	EEex_GetUDAux(sprite)["EEex_Fix_HasSpellOrSpellPointStartedCasting"] = 0
+end
--- EEex/copy/EEex_Fix_Patch.lua
+++ EEex/copy/EEex_Fix_Patch.lua
@@ -49,6 +49,105 @@
 		]]},
 	}))
 
+	----------------------------------------------------------------------------------
+	-- Fix Spell() and SpellPoint() not being disruptable if the creature is facing --
+	-- SSW(1), SWW(3), NWW(5), NNW(7), NNE(9), NEE(11), SEE(13), or SSE(15)         --
+	----------------------------------------------------------------------------------
+
+	--------------------------------------------------
+	-- EEex_Fix_Hook_ShouldForceMainSpellActionCode --
+	--------------------------------------------------
+
+	local callShouldForceMainSpellActionCode = EEex_JITNear(EEex_FlattenTable({
+		{[[
+			#STACK_MOD(8) ; This was called, the ret ptr broke alignment
+			#MAKE_SHADOW_SPACE(48)
+		]]},
+		EEex_GenLuaCall("EEex_Fix_Hook_ShouldForceMainSpellActionCode", {
+			["args"] = {
+				function(rspOffset) return {"mov qword ptr ss:[rsp+#$(1)], rcx #ENDL", {rspOffset}}, "CGameSprite" end,
+				function(rspOffset) return {"mov qword ptr ss:[rsp+#$(1)], rdx #ENDL", {rspOffset}}, "CPoint" end,
+			},
+			["returnType"] = EEex_LuaCallReturnType.Boolean,
+		}),
+		{[[
+			jmp no_error
+
+			call_error:
+			xor rax, rax
+
+			no_error:
+			#DESTROY_SHADOW_SPACE
+			ret
+		]]},
+	}))
+
+	EEex_HookJumpOnFail(EEex_Label("Hook-CGameSprite::Spell()-CheckDirectionJmp"), 3, {[[
+		mov rdx, r14
+		mov rcx, rbx
+		call short #$(1) ]], {callShouldForceMainSpellActionCode}, [[ #ENDL
+		test rax, rax
+		jnz #L(jmp_success)
+	]]})
+
+	EEex_HookJumpOnFail(EEex_Label("Hook-CGameSprite::SpellPoint()-CheckDirectionJmp"), 5, {[[
+		lea rdx, qword ptr ss:[rsp+0x60]
+		mov rcx, rbx
+		call short #$(1) ]], {callShouldForceMainSpellActionCode}, [[ #ENDL
+		test rax, rax
+		jnz #L(jmp_success)
+	]]})
+
+	---------------------------------------------------------
+	-- EEex_Fix_Hook_OnSpellOrSpellPointStartedCastingGlow --
+	---------------------------------------------------------
+
+	local callOnSpellOrSpellPointStartedCastingGlow = EEex_JITNear(EEex_FlattenTable({
+		{[[
+			#STACK_MOD(8) ; This was called, the ret ptr broke alignment
+			#MAKE_SHADOW_SPACE(40)
+		]]},
+		EEex_GenLuaCall("EEex_Fix_Hook_OnSpellOrSpellPointStartedCastingGlow", {
+			["args"] = {
+				function(rspOffset) return {"mov qword ptr ss:[rsp+#$(1)], rcx #ENDL", {rspOffset}}, "CGameSprite" end,
+			},
+		}),
+		{[[
+			call_error:
+			#DESTROY_SHADOW_SPACE
+			ret
+		]]},
+	}))
+
+	for _, address in ipairs({
+		EEex_Label("Hook-CGameSprite::Spell()-ApplyCastingEffect()"),
+		EEex_Label("Hook-CGameSprite::SpellPoint()-ApplyCastingEffect()")
+	}) do
+		EEex_HookAfterCall(address, {[[
+			mov rcx, rbx
+			call short #$(1) ]], {callOnSpellOrSpellPointStartedCastingGlow}, [[ #ENDL
+		]]})
+	end
+
+	-----------------------------------
+	-- EEex_Fix_Hook_OnSetCurrAction --
+	-----------------------------------
+
+	EEex_HookAfterCall(EEex_Label("Hook-CGameSprite::SetCurrAction()-FirstCall"), EEex_FlattenTable({
+		{[[
+			#MAKE_SHADOW_SPACE(40)
+		]]},
+		EEex_GenLuaCall("EEex_Fix_Hook_OnSetCurrAction", {
+			["args"] = {
+				function(rspOffset) return {"mov qword ptr ss:[rsp+#$(1)], rdi #ENDL", {rspOffset}}, "CGameSprite" end,
+			},
+		}),
+		{[[
+			call_error:
+			#DESTROY_SHADOW_SPACE
+		]]},
+	}))
+
     EEex_EnableCodeProtection()
 
 end)()
