155bfa852a43ff24bc2dd95f28b3130297c27d2e
Bubb13 <36863623+Bubb13@users.noreply.github.com>
2023-01-28 15:31:41 -0800

    v0.9.17-alpha

--- package_mod.bat
+++ package_mod.bat
@@ -6,7 +6,7 @@ call "%~dp0..\ModPackaging\utilities\ie_games.bat"
 
 REM /* MODIFY: set the values of the 3 variables below to reflect the current mod version */
 set "mod_name=EEex"
-set "mod_version=master"
+set "mod_version=v0.9.17-alpha"
 set mod_folder=EEex
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
