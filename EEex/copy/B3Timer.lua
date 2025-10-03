0accb532d7f90dc460bb3f4a4b2bea73f9af878b
Renegade0 <havlic.petr@gmail.com>
2025-10-04 00:17:07 +0200

    Infinity UI++ modern hud compatibility

--- EEex/copy/B3Timer.lua
+++ EEex/copy/B3Timer.lua
@@ -169,6 +169,12 @@ function B3Timer_Private_Menu_Tick()
 					local curX = portraitInstanceCurX[i]
 					if not curX then
 						local startX = (B3Timer_Private_HugPortraits:get() == 1 and portraitArea.x or 0) - 3
+						
+						--Infinity UI++ modern hud compatibility
+						if RgUISkin and RgIsModernHud == 1 then
+							startX = portraitArea.x + portraitArea.w
+						end
+						
 						curX = startX
 						portraitInstanceStartX[i] = startX
 						portraitInstanceCurX[i] = curX
@@ -176,9 +182,18 @@ function B3Timer_Private_Menu_Tick()
 
 					local portraitEnabledFunc = instanceEntry.portraitEnabledFunc
 					if (not portraitEnabledFunc or portraitEnabledFunc()) and condition then
+					
+						--Infinity UI++ modern hud compatibility
+						local rgDif = 3
+						local rgWidth = nil
+						if RgUISkin and RgIsModernHud == 1 then
+							rgDif = 2
+							rgWidth = 1
+						end
+						
 						instanceEntry.enabled = true
-						portraitInstanceCurX[i] = curX - 3
-						EEex_Menu_SetTemplateArea(B3Timer_Private_InjectingMenu, templateName, instanceEntry.id, curX, portraitArea.y, nil, portraitArea.h)
+						portraitInstanceCurX[i] = curX - rgDif
+						EEex_Menu_SetTemplateArea(B3Timer_Private_InjectingMenu, templateName, instanceEntry.id, curX, portraitArea.y, rgWidth, portraitArea.h)
 					else
 						instanceEntry.enabled = false
 					end
@@ -195,7 +210,14 @@ function B3Timer_Private_Menu_Tick()
 				if curX ~= startX then
 					backgroundEntry.enabled = true
 					local portraitArea = backgroundEntry.portraitItem.area
-					EEex_Menu_SetTemplateArea(B3Timer_Private_InjectingMenu, "B3Timer_Menu_TEMPLATE_Background", backgroundEntry.id, curX + 2, portraitArea.y, startX - curX + 1, portraitArea.h)
+					
+					--Infinity UI++ modern hud compatibility
+					local rgDif = 1
+					if RgUISkin and RgIsModernHud == 1 then
+						rgDif = 0
+					end
+					
+					EEex_Menu_SetTemplateArea(B3Timer_Private_InjectingMenu, "B3Timer_Menu_TEMPLATE_Background", backgroundEntry.id, curX + 2, portraitArea.y, startX - curX + rgDif, portraitArea.h)
 				else
 					backgroundEntry.enabled = false
 				end
